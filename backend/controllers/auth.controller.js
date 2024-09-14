f70671660e50bf1ece847ee382d54e5909fd030b
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-09-14 11:35:30 +0530

    create  models

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,19 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req,res)=>{
-    res.send("sign up route called")
+    const {email,password,name}=req.body
+    try {
+        const userExist=await User.findOne({email});
+
+        if(userExist){
+            res.status(400).json({message:"User already exist"});
+        }
+        const user=await User.create({name,email,password})
+    
+        res.status(201).json({user,message:"User created successfully"});
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
 };
 
 export const login = async (req,res)=>{
