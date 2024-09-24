fef12a9ada54ca41b3a4968f9edc52cb071bd404
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-09-24 10:44:43 +0530

    create coupon validation

---
+++ backend/models/coupon.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+ 
+const couponSchema= new mongoose.Schema(
+    {
+        code:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        discountPercentage:{
+            type:Number,
+            required:true,
+            min:0,
+            min100,
+        },
+        expirationData:{
+            type:Date,
+            required:true,
+        },
+        isActive:{
+            type:Boolean,
+            default:true,
+        },
+        userId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true,
+            unique:true,
+        },
+    },
+        {
+            timestamps:true,
+        }
+    
+);
+export const Coupon=mongoose.model("Coupen",couponSchema);
+
+export default Coupon;
