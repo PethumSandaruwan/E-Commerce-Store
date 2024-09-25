b736ba4f3bced55d3dda280b8a57636c5f25f1bf
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-09-26 01:31:23 +0530

    create login page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Navbar = () => {
     const user = false;
-    const isAdmin = true;
+    const isAdmin = false;
     return (
         <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
             <div className="container mx-auto px-4 py-3 flex justify-between items-center">
