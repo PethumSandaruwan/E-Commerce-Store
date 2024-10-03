99ec5a13c6fd14d2d71878f9345a9edd6fd5fb58
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-10-03 22:21:41 +0530

    update homepage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,13 @@
+const catergories=[
+  {href:"/jeans",name:"Jeans",imageUrl:"/jeans.jpg"},
+  {href:"/tshirts",name:"T-shirts",imageUrl:"/tshirts.jpg"},
+  {href:"/shoes",name:"Shoes",imageUrl:"/shoes.jpg"},
+  {href:"/glasses",name:"Glasses",imageUrl:"/glasses.jpg"},
+  {href:"/jackets",name:"Jackets",imageUrl:"/jackets.jpg"},
+  {href:"/bags",name:"Bags",imageUrl:"/bags.jpg"},
+]
+
+
 import React from 'react'
 
 const HomePage = () => {
