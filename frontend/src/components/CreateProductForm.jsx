5906a8f7301ffb151a3e2c60247f17e7a035d413
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-10-09 01:11:49 +0530

    updated createProductForm

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { PlusCircle,Upload,Loader } from 'lucide-react';
 import { useProductStore } from '../stores/useProductStore';
 
-const catergories=["jean","t-shirt","shoes","glasses","jacket","suit","bag"];
+const catergories=["jean","t-shirts","shoes","glasses","jackets","suits","bags"];
 
 const CreateProductForm = () => {
   const [newProduct,setNewProduct]=useState({
