a0fc27b556349037c26c09ccc6bed73406f0a6fb
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-10-11 00:37:34 +0530

    changes in cartcontroller

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -56,7 +56,7 @@ export const removeAllFromCart = async(req,res)=>{
 export const updateQuantity=async(req,res)=>{
     try {
         const {id:productId}=req.params;
-        const {quantity}=re.body;
+        const {quantity}=req.body;
         const user= req.user;
         const existingItem = user.cartItems.find((item)=>item.id===productId)
         
