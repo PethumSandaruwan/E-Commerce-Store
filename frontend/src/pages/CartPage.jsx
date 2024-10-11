0ed060b1fc7a3b2a3965a7fb574607c4c52347cb
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-10-12 01:20:23 +0530

    updated cartpage

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -6,6 +6,7 @@ import { useCartStore } from '../stores/useCartStore'
 import { Link } from 'react-router-dom';
 import CartItem from '../components/CartItem';
 import PeopleAlsoBought from '../components/PeopleAlsoBought';
+import OrderSummary from '../components/OrderSummary';
 
 const CartPage = () => {
     const {cart}=useCartStore();
@@ -34,6 +35,20 @@ const CartPage = () => {
                 )}
                 {cart.length>0 && <PeopleAlsoBought/>}
             </motion.div>
+            {cart.length>0&&(
+              <motion.div
+              className='mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full'
+              initial={{opacity:0,x:20}}
+              animate={{opacity:1,x:0}}
+              transition={{duration:0.5,delay:0.4}}
+              
+              >
+               
+               <OrderSummary/>
+
+              </motion.div>
+            )}
+
 
             </div>
         </div>
