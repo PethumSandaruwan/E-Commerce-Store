840121d5b062339854551913aecc2e5d682f5b5a
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-10-12 01:20:04 +0530

    updated order summery

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const OrderSummary = () => {
   return (
-    <div>
+    <motion.div
+    className='space-y-4 rounded-lg border border-gray-700 bg-gray-800 p-4 shadow-sm sm:p-6'
+    initial={{opacity:0,y:20}}
+    animate={{opacity:1,y:0}}
+    transition={{duration:0.5}}
+    >
       
-    </div>
+    </motion.div>
   )
 }
 
