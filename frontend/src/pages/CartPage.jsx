998497d233d8f49895464adfbd2eb25f5e496aa6
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-10-15 14:33:02 +0530

    updated cartpage

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -13,7 +13,7 @@ const CartPage = () => {
 
   return (
     <div className='py-8 md:py-16'>
-        <div className='mx-auto ,max-w-screen-xl px-4 2xl:px-0'>
+        <div className='mx-auto max-w-screen-xl px-4 2xl:px-0'>
             <div className='mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8'>
             <motion.div
             className='mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl'
@@ -75,7 +75,7 @@ const EmptyCartUI = () => (
         className="px-6 py-2 mt-4 text-white transition-colors rounded-md bg-emerald-500 hover:bg-emerald-600"
         to={"/"}
       >
-        Star Shopping
+        Start Shopping
       </Link>
     </motion.div>
   );
